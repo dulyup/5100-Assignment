7079cb78278c7c82131f0b1b8e30e2a5c8dd83ab
dulyup <31580159+dulyup@users.noreply.github.com>
2017-10-06 15:56:12 -0700

    Add files via upload

---
+++ Assignment4/Task3/IpAddress.java
@@ -0,0 +1,37 @@
+package com.assign4;
+
+import java.util.ArrayList;
+
+public class IpAddress {
+    private String dottedDecimal;
+    private ArrayList<Integer> octets = new ArrayList();
+
+    public IpAddress(String dottedDecimal) {
+        this.dottedDecimal = dottedDecimal;
+        generateOctetsArray(dottedDecimal);
+    }
+
+    public void generateOctetsArray(String dottedDecimal){
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < dottedDecimal.length(); i++) {
+
+            if (dottedDecimal.charAt(i) == '.') {
+                octets.add(Integer.parseInt(sb.toString()));
+                sb.delete(0, sb.length());
+                continue;
+            }
+
+            sb.append(dottedDecimal.charAt(i));
+
+        }
+        octets.add(Integer.parseInt(sb.toString()));
+    }
+
+    public int getOctet(int position) {
+        return octets.get(position - 1);
+    }
+
+    public String getDottedDecimal() {
+        return dottedDecimal;
+    }
+}
