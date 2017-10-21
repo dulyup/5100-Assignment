19563f20b58a8031643dcf35e10144463efedff1
dulyup <31580159+dulyup@users.noreply.github.com>
2017-10-20 23:32:25 -0700

    Add files via upload

---
+++ Assignment6/Task2.java
@@ -0,0 +1,34 @@
+package com.assign6;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+public class Task2 {
+
+    public static void parse(File file) {
+        RandomAccessFile input = null;
+        String line = null;
+
+        try {
+            input = new RandomAccessFile(file, "r");
+            while ((line = input.readLine()) != null) {
+                System.out.println(line);
+            }
+            return;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (input != null) {
+                try {
+                    input.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
