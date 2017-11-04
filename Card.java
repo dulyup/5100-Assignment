162d62d92467190243ebc736d7d576e0e0582e6b
dulyup <31580159+dulyup@users.noreply.github.com>
2017-11-03 21:04:11 -0700

    Add files via upload

---
+++ Card.java
@@ -0,0 +1,80 @@
+package com.project2;
+
+public class Card {
+
+    private int num;
+    private int suit;
+
+    public Card(int num, int suit) {
+        this.num = num;
+        this.suit = suit;
+    }
+
+    public String getNumString() {
+        switch (num) {
+            case 2:
+                return "2";
+            case 3:
+                return "3";
+            case 4:
+                return "4";
+            case 5:
+                return "5";
+            case 6:
+                return "6";
+            case 7:
+                return "7";
+            case 8:
+                return "8";
+            case 9:
+                return "9";
+            case 10:
+                return "10";
+            case 11:
+                return "Jack";
+            case 12:
+                return "Queen";
+            case 13:
+                return "King";
+            case 14:
+                return "Ace";
+            default:
+                return "?";
+        }
+    }
+
+    public String getSuitString() {
+        switch (suit) {
+            case 0:
+                return "Clubs";
+            case 1:
+                return "Diamonds";
+            case 2:
+                return "Hearts";
+            case 3:
+                return "Spades";
+            default:
+                return "?";
+        }
+    }
+
+    @Override
+    public String toString() {
+        String str = "";
+        str += getNumString() + " of " + getSuitString();
+        return str;
+    }
+
+    public void display() {
+        System.out.println(toString());
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public int getSuit() {
+        return suit;
+    }
+}
+
