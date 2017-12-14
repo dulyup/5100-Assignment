ea15a2d2801f5653e2da52c225146df2a16a4590
Harsha Vykunta <harshavardhan.biit@gmail.com>
2017-12-13 16:20:03 -0800

    score update

--- Project2/GameDriver.java
+++ Project2/GameDriver.java
@@ -1,3 +1,7 @@
+/* Good Work
+ * score 18
+ * you are setting spades as shortest even though it contains Ace or queen or king. check the requirements. You total computed points are equaling to 25 which should be equal to 26.
+ */
 package com.project2;
 
 public class GameDriver {
